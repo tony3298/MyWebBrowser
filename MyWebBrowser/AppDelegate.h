6ed104712da5accbaca89081e14ea4b9c55ce19f
tony3298 <tonydakhoul@yahoo.com>
2015-05-13 18:55:51 -0500

    Initial Commit

---
+++ MyWebBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWebBrowser
+//
+//  Created by Tony Dakhoul on 5/13/15.
+//  Copyright (c) 2015 Tony Dakhoul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
